02726b460a76a0c1982175c029aad68b1aa2b47f
willjasen <willjasen@gmail.com>
2016-05-30 19:09:22 -0400

    copied from user.rb

---
+++ lib/pocketsmith/account.rb
@@ -0,0 +1,38 @@
+ACCOUNT_URL = "#{API_URL}/accounts"
+
+module Pocketsmith
+  class Account
+  
+    attr_reader :id
+  
+    def initialize(attributes)
+      @id = attributes["id"]
+      @login = attributes["login"]
+      @name = attributes["name"]
+      @email = attributes["email"]
+      @avatar_url = attributes["avatar_url"]
+      @beta_user = attributes["beta_user"]
+      @time_zone = attributes["time_zone"]
+      @week_start_day = attributes["week_start_day"]
+      @base_currency_code = attributes["base_currency_code"]
+      @always_show_base_currency = attributes["always_show_base_currency"]
+      @using_multiple_currencies = attributes["using_multiple_currencies"]
+    end  
+
+    def self.find(id)
+      conn = Faraday.new("#{USER_URL}/#{id}")
+      conn.authorization("Key", ENV["api_key"])
+      response = conn.get
+      attributes = JSON.parse(response.body)
+      new(attributes)
+    end
+
+    def self.me
+      conn = Faraday.new("#{ME_URL}")
+      conn.authorization("Key", ENV["api_key"])
+      response = conn.get  
+      attributes = JSON.parse(response.body)
+      new(attributes)
+    end
+  end
+end
